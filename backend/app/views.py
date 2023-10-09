3a245a89fa3c67c5700a2417ac976aeaadeab29e
unohayato <mamidori1p1p@gmail.com>
2023-10-09 23:05:41 +0900

    :sparkles: add ViewSet

--- backend/app/views.py
+++ backend/app/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+from .models import CurryShop, Food
+from .serializers import CurryShopSerializer, FoodSerializer
 
 # Create your views here.
+
+class FoodViewSet(viewsets.ModelViewSet):
+  queryset = Food.objects.all()
+  serializer_class = FoodSerializer
+
+class CurryShopViewSet(viewsets.ModelViewSet):
+  queryset = CurryShop.objects.all()
+  serializer_class = CurryShopSerializer
