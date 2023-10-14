ff480fe6d9544a2fd51a2a5887b61d8c447c3eb2
unohayato <mamidori1p1p@gmail.com>
2023-10-14 22:30:28 +0900

    :fire:

---
+++ frontend/src/components/DrfApiFetch.js
@@ -0,0 +1,12 @@
+import React, {useState, useEffect} from "react";
+// import axios from 'axios';
+
+const DrfApiFetch = () => {
+  return (
+    <div>
+      <h1>DRF API</h1>
+    </div>
+  )
+}
+
+export default DrfApiFetch;
