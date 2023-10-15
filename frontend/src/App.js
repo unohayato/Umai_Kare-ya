b3a2461ebfa0ed590723c3a637a78f3592b0d891
unohayato <mamidori1p1p@gmail.com>
2023-10-15 18:03:38 +0900

    :sparkles: add MUI Paper

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import ButtonAppBar from './components/Header';
+import DrfApiFetch from './components/DrfApiFetch';
 
 function App() {
   return (
     <div>
       <ButtonAppBar />
+      <DrfApiFetch />
     </div>
   );
 }
