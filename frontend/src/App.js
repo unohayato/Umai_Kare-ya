0e086b50c01d482d623899e7c15258872d43d6ed
unohayato <mamidori1p1p@gmail.com>
2023-10-16 18:37:08 +0900

    :sparkles: add CurryShop component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import ButtonAppBar from './components/Header';
 import DrfApiFetch from './components/DrfApiFetch';
+import CurryShop from './CurryShop';
 
 function App() {
   return (
     <div>
       <ButtonAppBar />
       <DrfApiFetch />
+      <CurryShop />
     </div>
   );
 }
---
+++ frontend/src/CurryShop.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const CurryShop = () => {
+  return (
+    <div>CurryShop</div>
+  )
+}
+
+export default CurryShop
