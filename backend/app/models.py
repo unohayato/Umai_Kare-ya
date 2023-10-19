c8067fab2d731ba210a5c7e74de777f6ebc95985
unohayato <mamidori1p1p@gmail.com>
2023-10-19 21:52:53 +0900

    :fire:

--- backend/app/models.py
+++ backend/app/models.py
@@ -1,29 +1,5 @@
 from django.db import models
 import datetime
-
-from django.db.models.query import QuerySet
-
-# Create your models here.
-
-
-class SoftDeletionManager(models.Manager):
-  def get_queryset(self):
-    return super().get_queryset().filter(deleted_at__is_null = True)
-
-class SoftDeletionModel(models.Model):
-  created_at = models.DateTimeField(verbose_name="作成日時", auto_now_add=True)
-  updated_at = models.DateTimeField(verbose_name="更新日時", auto_now=True)
-  deleted_at = models.DateField(verbose_name="削除日時", blank=True, null=True)
-  
-  class Meta:
-    abstract = True
-    
-  def delete(self):
-    self.deleted_at = datetime.datetime.now()
-    self.save()
-    
-  def hard_delete(self):
-    super().delete()
   
 
 class Food(SoftDeletionModel):
