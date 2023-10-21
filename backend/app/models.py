cef71326d04d35f48629fbc4f2e25494e16e9621
unohayato <mamidori1p1p@gmail.com>
2023-10-21 20:22:48 +0900

    :sparkles: change models

--- backend/app/models.py
+++ backend/app/models.py
@@ -27,25 +27,25 @@ class SoftDeletionModel(models.Model):
   
 
 class Food(SoftDeletionModel):
-  EVALUATION = (
-            (0, "うまい!"),
-            (1, "うま!"),
-            (2, "うっま!"),
-            (3, "うまっ!"),
-            (4, "うまーい!"),
-            (5, "うまー!"),
-  )
-  name = models.CharField(verbose_name="メニュー名", max_length=255)
-  price = models.IntegerField(verbose_name="金額")
-  evaluation = models.IntegerField(verbose_name="評価", choices=EVALUATION)
+    EVALUATION = (
+        (0, "うまい!"),
+        (1, "うま!"),
+        (2, "うっま!"),
+        (3, "うまっ!"),
+        (4, "うまーい!"),
+        (5, "うまー!"),
+    )
+    name = models.CharField(verbose_name="メニュー名", max_length=255)
+    price = models.IntegerField(verbose_name="金額")
+    evaluation = models.IntegerField(verbose_name="評価", choices=EVALUATION)
+    curry_shop = models.ForeignKey('CurryShop', verbose_name="カレーショップ", on_delete=models.CASCADE)
 
-  def __str__(self):
-    return self.name
+    def __str__(self):
+        return self.name
 
 class CurryShop(SoftDeletionModel):
-  name = models.CharField(verbose_name="店名", max_length=255)
-  address = models.CharField(verbose_name="場所", max_length=255)
-  foods = models.ForeignKey(Food, verbose_name="食べたもの", on_delete=models.CASCADE)
-  
-  def __str__(self):
-    return self.name
+    name = models.CharField(verbose_name="店名", max_length=255)
+    address = models.CharField(verbose_name="場所", max_length=255)
+
+    def __str__(self):
+        return self.name
