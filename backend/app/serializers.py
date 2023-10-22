b549ccb14176b8a0a16d24cd218b7cdf7bfdb382
unohayato <mamidori1p1p@gmail.com>
2023-10-22 22:27:05 +0900

    :bulb: fix serializer

--- backend/app/serializers.py
+++ backend/app/serializers.py
@@ -4,9 +4,9 @@ from .models import CurryShop, Food
 class FoodSerializer(serializers.ModelSerializer):
   class Meta:
     model = Food
-    fields = ['id', 'name', 'price', 'evaluation']
+    fields = ['id', 'name', 'price', 'evaluation', 'curry_shop']
 
 class CurryShopSerializer(serializers.ModelSerializer):
   class Meta:
     model = CurryShop
-    fields = ['id', 'name', 'address', 'foods']
+    fields = ['id', 'name', 'address']
