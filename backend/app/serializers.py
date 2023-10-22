5bbce549e1306910a12297ee9e8f893e8edb3f9a
unohayato <mamidori1p1p@gmail.com>
2023-10-22 22:33:37 +0900

    :bulb: change the notation of fields of model

--- backend/app/serializers.py
+++ backend/app/serializers.py
@@ -2,9 +2,12 @@ from rest_framework import serializers
 from .models import CurryShop, Food
 
 class FoodSerializer(serializers.ModelSerializer):
+  evaluation_display = serializers.CharField(source='get_evaluation_display', read_only=True)
+  curry_shop_name = serializers.CharField(source='curry_shop.name', read_only=True)
+
   class Meta:
-    model = Food
-    fields = ['id', 'name', 'price', 'evaluation', 'curry_shop']
+      model = Food
+      fields = ['id', 'name', 'price', 'evaluation', 'evaluation_display', 'curry_shop', 'curry_shop_name']
 
 class CurryShopSerializer(serializers.ModelSerializer):
   class Meta:
