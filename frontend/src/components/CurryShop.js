d7dc70d1a0dcac495e312931eb18a485f7f4f8c5
unohayato <mamidori1p1p@gmail.com>
2023-10-22 23:46:04 +0900

    :sparkles: add shop

--- frontend/src/components/CurryShop.js
+++ frontend/src/components/CurryShop.js
@@ -1,12 +1,21 @@
 import React, {useState, useEffect} from "react";
+import { useNavigate } from 'react-router-dom'; // 必要なimportを追加
 import axios from 'axios';
 
+import ButtonAppBar from '../components/Header';
 import Box from '@mui/material/Box';
 import Paper from '@mui/material/Paper';
 import '../Font.css';
 
 const CurryShop = () => {
   const [shops, setShops] = useState([]);
+
+  const navigate = useNavigate();
+
+  // 店の詳細ページへの遷移をハンドルする関数
+  const handleShopClick = (shopId) => {
+    navigate(`/${shopId}/foods`);
+  };
 
   useEffect(() => {
     axios.get('http://localhost/api/curry_shops/', {
@@ -20,28 +29,34 @@ const CurryShop = () => {
 
   return (
     <div>
+      <ButtonAppBar />
       <div className="title">
         <h1>うまいカレー屋一覧</h1>
       </div>
       <Box
-        sx={{
-          display: 'flex',
-          flexWrap: 'wrap',
-          '& > :not(style)': {
-            m: 1,
-            width: 128, // またはコンテンツに合わせてサイズを調整
-            height: 128, // またはコンテンツに合わせてサイズを調整
-          },
-        }}
-      >
-        {shops.map((shop) => (
-          <Paper key={shop.id} elevation={3} sx={{ padding: 2 }}> {/* paddingはコンテンツに合わせて調整 */}
-            <div>店名: {shop.name}</div>
-            <div>住所: {shop.address}</div>
-            <div>食べたもの: {shop.foods}</div>
-          </Paper>
-        ))}
-      </Box>
+      sx={{
+        display: 'flex',
+        flexWrap: 'wrap',
+        '& > :not(style)': {
+          m: 1,
+          width: 128, // またはコンテンツに合わせてサイズを調整
+          height: 128, // またはコンテンツに合わせてサイズを調整
+        },
+      }}
+    >
+      {shops.map((shop) => (
+        <Paper 
+          key={shop.id} 
+          elevation={3} 
+          sx={{ padding: 2 }} // paddingはコンテンツに合わせて調整
+          onClick={() => handleShopClick(shop.id)} // ここでクリックイベントを追加
+          style={{ cursor: 'pointer' }} // マウスカーソルをポインターに変更
+        >
+          <div>店名: {shop.name}</div>
+          <div>住所: {shop.address}</div>
+        </Paper>
+      ))}
+    </Box>
     </div>
   )
 }
